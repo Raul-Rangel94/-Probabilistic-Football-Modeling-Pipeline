cbbe2476db95eb3675536b75b0ae7ab711e6c321
Raul Rangel <blackpointdlx@gmail.com>
2025-12-28 21:07:17 -0600

    Refactor compute_form to use rolling average of points
    Updated compute_form function to calculate a normalized rolling average of points instead of using log transformation of goals.

--- src/features/form.py
+++ src/features/form.py
@@ -1,12 +1,23 @@
 import numpy as np
 import pandas as pd
 
-def compute_form(df: pd.DataFrame) -> pd.DataFrame:
-    '''
-    Compute team offensive form using log transformation.
-    '''
+def compute_form(
+    df: pd.DataFrame,
+    window: int = 5
+) -> pd.DataFrame:
+    """
+    Compute team form as normalized rolling average of points.
+    """
 
-    df.copy()
+    df = df.copy()
+    df = df.sort_values(['team', 'date'])
 
-    df ['form'] = np.log1p(df['goals_for'])
+    df['form'] = (
+        df
+        .groupby('team')['points']
+        .transform(
+            lambda x: x.rolling(window=window, min_periods=1).sum() / (3 * window)
+        )
+    )
+
     return df
